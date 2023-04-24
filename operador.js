8f20451022c5629fa7b865cf1f6bc872f3dcd35b
vivianatejada01 <129109697+vivianatejada01@users.noreply.github.com>
2023-04-24 15:09:06 -0500

    main

---
+++ operador.js
@@ -0,0 +1,8 @@
+//CONVERSION LARGA: 
+var celsius = prompt ("Introduzca la temperatura en grados celsius: "); 
+celcius = parseFloat(celsius);
+
+//CONVERSION CORTA:
+var celcius = parseFloat(prompt("Introduzca la temperatura en grados celsius: "));
+var f = 9/5*+32;
+document.write("La conversion es: ", f); 
